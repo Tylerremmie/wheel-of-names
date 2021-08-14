466e1f227eddd1fa5015dc461ee7a98ef8694275
TylerRemmie <Tylerremmie@gmail.com>
2021-08-14 02:09:01 -0500

    Delivering v2 of wheel-of-names

--- js/WheelScript.js
+++ js/WheelScript.js
@@ -1,18 +1,28 @@
+const WHEEL_RADIUS = 400;
+const TEXT_FONT_SIZE = 50;
+
 // Create new wheel object specifying the parameters at creation time.
 let theWheel = new Winwheel({
-    'numSegments': 8,     // Specify number of segments.
-    'outerRadius': 212,   // Set outer radius so wheel fits inside the background.
-    'textFontSize': 28,    // Set font size as desired.
+    'numSegments': 15,     // Specify number of segments.
+    'outerRadius': WHEEL_RADIUS,   // Set outer radius so wheel fits inside the background.
+    'textFontSize': TEXT_FONT_SIZE,    // Set font size as desired.
     'segments':        // Define segments including colour and text.
         [
-            { 'fillStyle': '#eae56f', 'text': 'Prize 1' },
-            { 'fillStyle': '#89f26e', 'text': 'Prize 2' },
-            { 'fillStyle': '#7de6ef', 'text': 'Prize 3' },
-            { 'fillStyle': '#e7706f', 'text': 'Prize 4' },
-            { 'fillStyle': '#eae56f', 'text': 'Prize 5' },
-            { 'fillStyle': '#89f26e', 'text': 'Prize 6' },
-            { 'fillStyle': '#7de6ef', 'text': 'Prize 7' },
-            { 'fillStyle': '#e7706f', 'text': 'Prize 8' }
+            { fillStyle: '#eae56f', text: 'Akhil', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#89f26e', text: 'Andy', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#7de6ef', text: 'Avery', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#c100be', text: 'Jerome', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#e7706f', text: 'Juan', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#eae56f', text: 'Jia', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#dc89f3', text: 'Jiebo', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#89f26e', text: 'Junaid', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#7de6ef', text: 'Katie', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#e7706f', text: 'Leo', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#a128d8', text: 'Minh', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#d1b738', text: 'Nick', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#785ec4', text: 'Shirley', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#bf3529', text: 'Stephanie', id: Math.floor(Math.random() * Date.now()) },
+            { fillStyle: '#96af30', text: 'Tyler', id: Math.floor(Math.random() * Date.now()) },
         ],
     'animation':           // Specify the animation to use.
     {
@@ -20,29 +30,8 @@ let theWheel = new Winwheel({
         'duration': 15,
         'spins': 8,
         'callbackFinished': alertPrize,
-        'callbackSound': playSound,   // Function to call when the tick sound is to be triggered.
-        'soundTrigger': 'pin'        // Specify pins are to trigger the sound, the other option is 'segment'.
-    },
-    'pins':
-    {
-        'number': 16   // Number of pins. They space evenly around the wheel.
     }
 });
-
-// -----------------------------------------------------------------
-// This function is called when the segment under the prize pointer changes
-// we can play a small tick sound here like you would expect on real prizewheels.
-// -----------------------------------------------------------------
-let audio = new Audio('resources/tick.mp3');  // Create audio object and load tick.mp3 file.
-
-function playSound() {
-    // Stop and rewind the sound if it already happens to be playing.
-    audio.pause();
-    audio.currentTime = 0;
-
-    // Play the sound.
-    audio.play();
-}
 
 // -------------------------------------------------------
 // Called when the spin animation has finished by the callback feature of the wheel because I specified callback in the parameters
@@ -51,48 +40,16 @@ function playSound() {
 function alertPrize(indicatedSegment) {
     // Do basic alert of the segment text.
     // You would probably want to do something more interesting with this information.
-    alert("You have won " + indicatedSegment.text);
+    alert("The winner is: " + indicatedSegment.text);
+    resetWheel();
 }
 
 // =======================================================================================================================
 // Code below for the power controls etc which is entirely optional. You can spin the wheel simply by
 // calling theWheel.startAnimation();
 // =======================================================================================================================
-let wheelPower = 0;
+let wheelPower = 2;
 let wheelSpinning = false;
-
-// -------------------------------------------------------
-// Function to handle the onClick on the power buttons.
-// -------------------------------------------------------
-function powerSelected(powerLevel) {
-    // Ensure that power can't be changed while wheel is spinning.
-    if (wheelSpinning == false) {
-        // Reset all to grey incase this is not the first time the user has selected the power.
-        document.getElementById('pw1').className = "";
-        document.getElementById('pw2').className = "";
-        document.getElementById('pw3').className = "";
-
-        // Now light up all cells below-and-including the one selected by changing the class.
-        if (powerLevel >= 1) {
-            document.getElementById('pw1').className = "pw1";
-        }
-
-        if (powerLevel >= 2) {
-            document.getElementById('pw2').className = "pw2";
-        }
-
-        if (powerLevel >= 3) {
-            document.getElementById('pw3').className = "pw3";
-        }
-
-        // Set wheelPower var used when spin button is clicked.
-        wheelPower = powerLevel;
-
-        // Light up the spin button by changing it's source image and adding a clickable class to it.
-        document.getElementById('spin_button').src = "resources/spin_on.png";
-        document.getElementById('spin_button').className = "clickable";
-    }
-}
 
 // -------------------------------------------------------
 // Click handler for spin button.
@@ -100,19 +57,6 @@ function powerSelected(powerLevel) {
 function startSpin() {
     // Ensure that spinning can't be clicked again while already running.
     if (wheelSpinning == false) {
-        // Based on the power level selected adjust the number of spins for the wheel, the more times is has
-        // to rotate with the duration of the animation the quicker the wheel spins.
-        if (wheelPower == 1) {
-            theWheel.animation.spins = 3;
-        } else if (wheelPower == 2) {
-            theWheel.animation.spins = 8;
-        } else if (wheelPower == 3) {
-            theWheel.animation.spins = 15;
-        }
-
-        // Disable the spin button so can't click again while wheel is spinning.
-        document.getElementById('spin_button').src = "resources/spin_off.png";
-        document.getElementById('spin_button').className = "";
 
         // Begin the spin animation by calling startAnimation on the wheel object.
         theWheel.startAnimation();
@@ -130,10 +74,150 @@ function resetWheel() {
     theWheel.stopAnimation(false);  // Stop the animation, false as param so does not call callback function.
     theWheel.rotationAngle = 0;     // Re-set the wheel angle to 0 degrees.
     theWheel.draw();                // Call draw to render changes to the wheel.
-
-    document.getElementById('pw1').className = "";  // Remove all colours from the power level indicators.
-    document.getElementById('pw2').className = "";
-    document.getElementById('pw3').className = "";
-
     wheelSpinning = false;          // Reset to false to power buttons and spin can be clicked again.
+}
+
+
+// -------------------------------------------------------
+// Name functionality.
+// -------------------------------------------------------
+
+let nameList = theWheel.segments.filter(segment => segment != null);
+
+// -------------------------------------------------------
+// Function for render the list of names.
+// -------------------------------------------------------
+function renderNames(todo) {
+    localStorage.setItem('nameList', JSON.stringify(nameList));
+
+    const list = document.querySelector('.js-name-list');
+    const item = document.querySelector(`[data-key='${todo.id}']`);
+
+    if (todo.deleted) {
+        item.remove();
+        if (nameList.length === 0) list.innerHTML = '';
+        return
+    }
+
+    const isChecked = todo.checked ? 'done' : '';
+    const node = document.createElement("li");
+    node.setAttribute('class', `todo-item ${isChecked}`);
+    node.setAttribute('data-key', todo.id);
+    node.innerHTML = `
+    <span>${todo.text}</span>
+    <input class="delete-todo js-delete-todo" type="image" src="https://img.icons8.com/fluency/48/fa314a/delete-sign.png"/>
+    `
+
+    if (item) {
+        list.replaceChild(node, item);
+    } else {
+        list.append(node);
+    }
+}
+
+// -------------------------------------------------------
+// Function for re-render the wheel after changes.
+// -------------------------------------------------------
+function renderWheel() {
+    theWheel = new Winwheel({
+        'numSegments': nameList.length,     // Specify number of segments.
+        'outerRadius': WHEEL_RADIUS,   // Set outer radius so wheel fits inside the background.
+        'textFontSize': TEXT_FONT_SIZE,    // Set font size as desired.
+        'segments': nameList,
+        'animation':           // Specify the animation to use.
+        {
+            'type': 'spinToStop',
+            'duration': 15,
+            'spins': 8,
+            'callbackFinished': alertPrize,
+        }
+    });
+}
+
+// -------------------------------------------------------
+// Function to add a name.
+// -------------------------------------------------------
+function addName(text) {
+    const name = {
+        text,
+        id: Date.now(),
+    };
+
+    nameList.push(name);
+    renderWheel();
+    renderNames(name);
+}
+
+// -------------------------------------------------------
+// Function to delete a name.
+// -------------------------------------------------------
+function deleteName(key) {
+    const index = nameList.findIndex(item => item.id === Number(key));
+    const name = {
+        deleted: true,
+        ...nameList[index]
+    };
+    nameList = nameList.filter(item => item.id !== Number(key));
+    renderNames(name);
+    renderWheel();
+}
+
+// -------------------------------------------------------
+// Event listener for submiting a name from the input.
+// -------------------------------------------------------
+const form = document.querySelector('.js-form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const input = document.querySelector('.js-name-input');
+
+    const text = input.value.trim();
+    if (text !== '') {
+        addName(text);
+        input.value = '';
+        input.focus();
+    }
+});
+
+// -------------------------------------------------------
+// Event listener for deleting a name from the list.
+// -------------------------------------------------------
+const list = document.querySelector('.js-name-list');
+list.addEventListener('click', event => {
+    console.log(event.target.classList);
+    if (event.target.classList.contains('js-delete-todo')) {
+        const itemKey = event.target.parentElement.dataset.key;
+        deleteName(itemKey);
+    }
+});
+
+// -------------------------------------------------------
+// Event listener for the page to load.
+// -------------------------------------------------------
+document.addEventListener('DOMContentLoaded', () => {
+    localStorage.setItem('nameList', JSON.stringify(nameList));
+    const ref = localStorage.getItem('nameList');
+    if (ref) {
+        nameList = JSON.parse(ref);
+        nameList.forEach(t => {
+            renderNames(t);
+        });
+    }
+});
+
+// -------------------------------------------------------
+// Event listener for opening and closing the collapsible list.
+// -------------------------------------------------------
+var coll = document.getElementsByClassName("collapsible-button");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
 }
